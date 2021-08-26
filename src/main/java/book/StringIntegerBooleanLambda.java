ac25459bfa34c4f21555ec1e89a342507a8dccd2
Robert Delacruz <robertdelacruz951@gmail.com>
2021-08-25 21:49:33 -0700

    initial commit with HelloWorld, ListMapLambda, and StringIntegerBooleanLambda

---
+++ src/main/java/book/StringIntegerBooleanLambda.java
@@ -0,0 +1,16 @@
+package book;
+
+public class StringIntegerBooleanLambda {
+    public String handlerString(String s) {
+        return "Hello,,, " + s;
+    }
+
+    public boolean handlerBoolean(boolean input) {
+        return !input;
+    }
+
+    public boolean handlerInt(int input) {
+        return input > 100;
+    }
+
+}
